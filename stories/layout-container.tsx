6344f227cee6b4058ebd581d2af1c82d00fd6aa1
Jason Gerbes <j.gerbes@me.com>
2025-01-28 11:10:46 +1300

    tweak styles

--- stories/layout-container.tsx
+++ stories/layout-container.tsx
@@ -9,14 +9,14 @@ export function LayoutContainer({ children, className }: LayoutContainerProps) {
   return (
     <>
       <button
-        className="rounded-sm text-center text-sm text-balance text-gray-500 outline-amber-400 focus:outline-2"
+        className="mb-2.5 rounded-sm px-1 text-center text-sm text-balance text-gray-500 outline-amber-400 focus:outline-2"
         tabIndex={0}
       >
         Click here, press <code>TAB</code>, then use <code>ARROW KEYS</code>
       </button>
       <div
         className={cn(
-          'mt-2.5 gap-2.5 rounded-2xl border border-gray-300 bg-gray-50 p-3',
+          'gap-2.5 rounded-2xl border border-gray-300 bg-gray-50 p-3',
           className,
         )}
       >
